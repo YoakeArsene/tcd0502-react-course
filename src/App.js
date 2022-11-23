dc19782bb647c7b1e08851e3b70a7f4e9cf8f4d2
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-11-23 08:58:29 +0700

    Introduction to JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  // Properties => state
+  state = {
+    fullName: "Vinh Hoang",
+    address: "Ngo Quyen",
+    counter: 0,
+    isDisplayed: true,
+  };
+
+  // Methods
+  increaseCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  decreaseCounter = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+
+  setDisplayed = () => {
+    this.setState({
+      isDisplayed: !this.state.isDisplayed,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.state.fullName}</h1>
+        {this.state.isDisplayed && <h2>Address: {this.state.address}</h2>}
+        <p>Counter: {this.state.counter}</p>
+        <button onClick={this.increaseCounter}>Increase Counter</button>
+        <button onClick={this.decreaseCounter}>Decrease Counter</button>
+        <button onClick={this.setDisplayed}>Set Display</button>
+      </div>
+    );
+  }
 }
 
 export default App;

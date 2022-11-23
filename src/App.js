aaa30015e2dd847bf67c7a50d56610d92784ced1
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-11-23 09:48:21 +0700

    Create 2 components with static data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
 import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import UserItem from "./components/users/UserItem";
 
 export class App extends Component {
-  // Properties => state
-  state = {
-    fullName: "Vinh Hoang",
-    address: "Ngo Quyen",
-    counter: 0,
-    isDisplayed: true,
-  };
-
-  // Methods
-  increaseCounter = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  };
-
-  decreaseCounter = () => {
-    this.setState({
-      counter: this.state.counter - 1,
-    });
-  };
-
-  setDisplayed = () => {
-    this.setState({
-      isDisplayed: !this.state.isDisplayed,
-    });
-  };
-
   render() {
     return (
       <div>
-        <h1>Hello {this.state.fullName}</h1>
-        {this.state.isDisplayed && <h2>Address: {this.state.address}</h2>}
-        <p>Counter: {this.state.counter}</p>
-        <button onClick={this.increaseCounter}>Increase Counter</button>
-        <button onClick={this.decreaseCounter}>Decrease Counter</button>
-        <button onClick={this.setDisplayed}>Set Display</button>
+        <Navbar />
+        <UserItem />
+        <UserItem />
+        <UserItem />
+        <UserItem />
+        <UserItem />
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="navbar bg-primary">TCD0502 React Course</div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;

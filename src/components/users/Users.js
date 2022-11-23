e5c84b80b064625502afd88cd29c0ed64b9aa844
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-11-23 10:25:25 +0700

    Use axios to send request

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,29 +1,20 @@
+import axios from "axios";
 import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class Users extends Component {
   state = {
-    usersData: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
+    usersData: [],
   };
+
+  componentDidMount() {
+    console.log("Users component mounted ...");
+    axios.get("https://api.github.com/users").then((response) => {
+      console.log(response.data);
+      this.setState({ usersData: response.data });
+    });
+  }
+
   render() {
     return (
       <div style={userStyle}>

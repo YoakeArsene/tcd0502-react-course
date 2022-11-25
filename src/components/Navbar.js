c66aab3e2b1a88812ea92a347a9c2ca2d8279282
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-11-25 10:10:30 +0700

    Convert Navbar, Users and UserItem to functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <div className="navbar bg-primary">TCD0502 React Course</div>
-      </div>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-primary">TCD0502 React Course</div>
+    </div>
+  );
+};
 
 export default Navbar;

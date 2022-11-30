8d19aac46b643ff334987a73bebb93a46a1e26bc
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-11-30 10:20:55 +0700

    Refactor: create api folder

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,5 +1,5 @@
-import axios from "axios";
 import { useReducer } from "react";
+import { getGithubUser, getGithubUsers } from "../../api/GithubApi";
 import { CLEAR_USERS, GET_USER, SEARCH_USERS } from "../types";
 import GithubContext from "./githubContext";
 import GithubReducer from "./githubReducer";
@@ -12,9 +12,7 @@ const GithubState = (props) => {
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   const searchUsers = async (text) => {
-    const response = await axios.get(
-      `https://api.github.com/search/users?q=${text}`
-    );
+    const response = await getGithubUsers(text);
     dispatch({
       type: SEARCH_USERS,
       payload: response.data.items,
@@ -28,7 +26,7 @@ const GithubState = (props) => {
   };
 
   const getUser = async (loginId) => {
-    const response = await axios.get(`https://api.github.com/users/${loginId}`);
+    const response = await getGithubUser(loginId);
     dispatch({
       type: GET_USER,
       payload: response.data,
